e775a08b9ddb40252f8d7e3e9f1f680efe0ac3e8
manish465 <ms5978307@gmail.com>
2020-02-29 22:39:16 -0800

    added dynamic burger genrator to the Burger components

--- src/container/BurgerBuilder/BurgerBuilder.jsx
+++ src/container/BurgerBuilder/BurgerBuilder.jsx
@@ -3,11 +3,18 @@ import Aux from "../../HOC/Auxiliary";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
-    state = {};
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 3,
+            mete: 2,
+        },
+    };
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
         );
