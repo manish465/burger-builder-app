1feafe4554d6c8bf3381c4e6902649d0fdb843af
manish465 <ms5978307@gmail.com>
2020-03-04 12:09:18 -0800

    error terminated

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -4,7 +4,7 @@ import styles from "./Button.module.css";
 const button = props => {
     return (
         <button
-            className={(styles.Button, styles[props.btnType])}
+            className={[styles.Button, styles[props.btnType]].join(" ")}
             onClick={props.click}
         >
             {props.children}
