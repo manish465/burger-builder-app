265df105ab706354ab1e40c24e3228a94b3a99f3
manish465 <ms5978307@gmail.com>
2020-03-04 12:02:33 -0800

    added coustom button

---
+++ src/components/UI/Button/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+const button = props => {
+    return (
+        <button
+            className={(styles.Button, styles[props.btnType])}
+            onClick={props.click}
+        >
+            {props.children}
+        </button>
+    );
+};
+
+export default button;
