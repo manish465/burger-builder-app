164343cd527fab537c59ac20625970920c669599
manish465 <ms5978307@gmail.com>
2020-03-07 12:46:06 -0800

    added sidedrawer to the layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Aux from "../../HOC/Auxiliary";
 import styles from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = props => {
     return (
         <Aux>
             <Toolbar />
+            <SideDrawer />
             <div className={styles.Content}>{props.children}</div>
         </Aux>
     );
